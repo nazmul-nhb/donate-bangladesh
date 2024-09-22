8fee624cf0666715c6881788ff9544ee9e28a359
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-23 05:18:06 +0600

    validated donation inputs

--- src/utilities/calculateDonation.ts
+++ src/utilities/calculateDonation.ts
@@ -1,11 +1,26 @@
 import { balanceContainer } from "../main";
 import { getElementByID } from "./getElements";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
 
 export const calculateDonation = (containerID: string, inputID: string) => {
 	const donationContainer = getElementByID(containerID);
 	const amount = Number((getElementByID(inputID) as HTMLInputElement).value);
 
 	let currentBalance = Number(balanceContainer?.innerText);
+
+	if (!amount) {
+		return toastr.error("Amount Cannot Be Empty!");
+	}
+
+	if (amount > currentBalance) {
+		return toastr.error("Insufficient Balance!");
+	}
+
+	if (amount < 0) {
+		return toastr.error("Invalid Donation Amount!");
+	}
+
 	currentBalance -= amount;
 
 	if (balanceContainer) {
@@ -16,5 +31,6 @@ export const calculateDonation = (containerID: string, inputID: string) => {
 			donationContainer.innerText = noakhaliNumberAmount.toString();
 		}
 		(getElementByID(inputID) as HTMLInputElement).value = "";
+		toastr.success(`Donated BDT ${amount}`);
 	}
 };
